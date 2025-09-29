867a25ab8c9b5e72dd9c60b51e9cc08abe009487
Mathmout <mathys1203@gmail.com>
2025-09-29 22:10:46 +0200

    Ajout d'un craft pour le Sheep Scanner.

--- src/main/java/com/mathmout/resourcefulsheep/datagen/DataGenerators.java
+++ src/main/java/com/mathmout/resourcefulsheep/datagen/DataGenerators.java
@@ -1,6 +1,6 @@
 package com.mathmout.resourcefulsheep.datagen;
 
-
+import com.mathmout.resourcefulsheep.datagen.recipe.ModRecipeProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
@@ -12,12 +12,12 @@ import java.util.concurrent.CompletableFuture;
 
 public class DataGenerators {
     @SubscribeEvent
-
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
+        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput, lookupProvider));
     }
 }
---
+++ src/main/java/com/mathmout/resourcefulsheep/datagen/recipe/ModRecipeProvider.java
@@ -0,0 +1,30 @@
+package com.mathmout.resourcefulsheep.datagen.recipe;
+
+import com.mathmout.resourcefulsheep.item.ModItems;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.Items;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends RecipeProvider {
+    public ModRecipeProvider(PackOutput pOutput, CompletableFuture<HolderLookup.Provider> pRegistries) {
+        super(pOutput, pRegistries);
+    }
+
+    @Override
+    protected void buildRecipes(@NotNull RecipeOutput pRecipeOutput) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.SHEEP_SCANNER.get())
+                .pattern("IGI")
+                .pattern("IRI")
+                .pattern("IDI")
+                .define('I', Items.IRON_INGOT)
+                .define('G', Items.GLASS)
+                .define('R', Items.REDSTONE)
+                .define('D', Items.DIAMOND)
+                .unlockedBy("has_iron_ingot", has(Items.IRON_INGOT))
+                .save(pRecipeOutput);
+    }
+}
